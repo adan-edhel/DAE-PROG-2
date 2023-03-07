a5d0640d450e35d0f1fc265326e6eaacd5c13108
Mort <44699487+adan-edhel@users.noreply.github.com>
2023-03-07 22:08:27 +0100

    Fixed MemoryLeakDump()

--- HollowKnight/main.cpp
+++ HollowKnight/main.cpp
@@ -2,9 +2,8 @@
 #include <ctime>
 #include "Game.h"
 
-
 void StartHeapControl();
-void DumpMemoryLeaks();
+int CustomizeReportHook(int reportType, char* message, int* returnValue);
 
 int SDL_main(int argv, char** args)
 {
@@ -12,14 +11,12 @@ int SDL_main(int argv, char** args)
 
 	StartHeapControl();
 
-	Game* pGame{ new Game{ Window{ "Hollow Knight - Onmaz, 'Mort' Yasin - 1DAE12", 1280 , 800 } } };
+	Game* pGame{ new Game{ Window{ "Hollow Knight - Onmaz, 'Mort' Yasin - 1DAE12", 1280.f , 800.f } } };
 	pGame->Run();
 	delete pGame;
 
-	DumpMemoryLeaks();
 	return 0;
 }
-
 
 void StartHeapControl()
 {
@@ -30,6 +27,9 @@ void StartHeapControl()
 	// Report detected leaks when the program exits
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
+	// Customize output of the dump
+	_CrtSetReportHook(CustomizeReportHook);
+
 	// Set a breakpoint on the specified object allocation order number
 	//_CrtSetBreakAlloc( 156 );
 #endif
@@ -38,12 +38,6 @@ void StartHeapControl()
 int CustomizeReportHook(int reportType, char* message, int* returnValue)
 {
 	if (reportType == _CRT_WARN) {
-
-		// Best method, as it will log actual red errors, but it has .NET dependency,
-		// which I'm not sure is worth it to add for this functionality.
-		//System.Diagnostics.Debug.WriteLine(">>Warning: Memory Leak!<< \n");
-
-		// Alternative without .NET dependency
 		OutputDebugStringA(">>Warning: Memory Leak!<< \n");
 		*returnValue = 0;
 		return 1;
@@ -51,12 +45,4 @@ int CustomizeReportHook(int reportType, char* message, int* returnValue)
 
 	// Return 0 to use the default report handler for other types of reports
 	return 0;
-}
-
-void DumpMemoryLeaks()
-{
-#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetReportHook(CustomizeReportHook);
-	_CrtDumpMemoryLeaks();
-#endif
 }
